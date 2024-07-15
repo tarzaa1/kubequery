d0a2307197554624009cc65abb9748ca15629696
Tarek Zaarour <tareqzaarour@gmail.com>
2024-07-15 11:26:13 +0100

    connection to neo4j

--- kubequery/run.py
+++ kubequery/run.py
@@ -1,4 +1,9 @@
-from kubequery.routes import app
+from kubequery.routes import app, neo4j
+from kubequery.utils.graph import Neo4j
 
 if __name__ == '__main__':
+    URI = "bolt://localhost:7687"
+    AUTH = ("neo4j", "password")
+    
+    neo4j.connect(URI, AUTH)
     app.run(host="0.0.0.0", port=5000, debug=True)
